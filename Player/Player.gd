82a7daaf1f04f08c249f105f50b92f6c9f836260
Nicholas Rodine <halfofastaple@gmail.com>
2019-01-26 15:19:47 -0500

    Added the ability to pick things up!

--- Player/Player.gd
+++ Player/Player.gd
@@ -13,3 +13,17 @@ func _physics_process(_delta):
 	if (Input.is_action_pressed("move_right")):
 		motion += basis.x;
 	$Ball.add_central_force(motion * speed);
+
+func _on_Ball_body_entered(body: Node):
+	if (body.is_in_group("collectable")):
+		var shape = body.get_node("CollisionShape") as CollisionShape;
+		var mesh = body.get_node("MeshInstance") as MeshInstance;
+		reparent_onto(shape, $Ball);
+		reparent_onto(mesh, $Ball);
+		body.queue_free();
+
+func reparent_onto(node: Spatial, new_parent: Spatial):
+	var previous_global = node.global_transform;
+	node.get_parent().remove_child(node);
+	new_parent.add_child(node);
+	node.global_transform = previous_global;
